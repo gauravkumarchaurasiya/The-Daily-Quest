1ee64e2d8698831857143e2c9730e678bea1f5f0
Gauravkumarchaurasiya <2002gauravkumarchaurasiya@gmail.com>
2023-07-15 21:12:04 +0530

    updated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function checkAnswer() {
     outputDiv.textContent = "Correct!";
     outputDiv.style.color = "green";
     descriptionDiv.style.display = "block";
-    likeButton.disabled = false;
+    // likeButton.disabled = false;
   } else {
     outputDiv.textContent = "Incorrect. Try again!";
     outputDiv.style.color = "red";
@@ -42,54 +42,116 @@ function checkAnswer() {
     }
   }
 }
-const checkAnswerButton = document.getElementById("checkAnswer");
-checkAnswerButton.addEventListener("click", checkAnswer);
-const toggleLikeButton = document.getElementById("toggleLike");
-toggleLikeButton.addEventListener("click", toggleLike);
-const toggleDarkModeButton = document.getElementById("toggleDarkMode");
-toggleDarkModeButton.addEventListener("click", toggleDarkMode);
+// const checkAnswerButton = document.getElementById("checkAnswer");
+// checkAnswerButton.addEventListener("click", checkAnswer);
+// const toggleLikeButton = document.getElementById("toggleLike");
+// toggleLikeButton.addEventListener("click", toggleLike);
+// const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+// toggleDarkModeButton.addEventListener("click", toggleDarkMode);
 
-function toggleLike() {
-  var likeButton = document.getElementById("likeButton");
-  likeButton.classList.toggle("active");
-}
+// const generateQuestion = async () => {
+//   const response = await fetch("https://gauravkumarchaurasiya.github.io/codeQuiz/python.json");
+//   const data = await response.json();
+//   const questionObjects = data;
 
-function toggleDarkMode() {
-  var body = document.body;
-  body.classList.toggle("dark-mode");
-}
-// document.addEventListener("DOMContentLoaded", function () {
-  const generateQuestion = async () => {
-    const response = await fetch(
-      "https://gauravkumarchaurasiya.github.io/codeQuiz/python.json"
-    );
-    const data = await response.json();
-    const questionObjects = data;
+//   // Get a random question object
+//   const randomQuestionObj = questionObjects[Math.floor(Math.random() * questionObjects.length)];
 
-    // Get a random question object
-    const randomQuestionObj =
-      questionObjects[Math.floor(Math.random() * questionObjects.length)];
+//   const question = randomQuestionObj.question;
+//   const codeSnippet = randomQuestionObj.code;
+//   const correctAnswer = randomQuestionObj.output;
+//   const description = randomQuestionObj.description;
 
-    const question = randomQuestionObj.question;
-    const codeSnippet = randomQuestionObj.code;
-    const correctAnswer = randomQuestionObj.output;
-    const description = randomQuestionObj.description;
+//   // Update the HTML elements
+//   document.getElementById("codeSnippet").textContent = codeSnippet;
+//   document.getElementById("correctAnswer").textContent = correctAnswer;
+//   document.getElementById("descriptionText").textContent = description;
 
-    // Update the HTML elements
-    document.getElementById("codeSnippet").textContent = codeSnippet;
-    document.getElementById("correctAnswer").textContent = correctAnswer;
-    document.getElementById("descriptionText").textContent = description;
+//   // Retrieve the previously saved favorite questions from localStorage
+//   const savedFavoriteQuestions = localStorage.getItem("favoriteQuestions");
+//   if (savedFavoriteQuestions) {
+//       const savedQuestionsSet = new Set(JSON.parse(savedFavoriteQuestions));
+//       questionSet.clear();
+//       savedQuestionsSet.forEach((question) => {
+//           questionSet.add(question);
+//       });
+//       updateFavoriteQuestions();
+//   }
 
-    return question;
-  };
+//   return question;
+// };
 
-  // Example usage
-  generateQuestion()
-    .then((result) => {
-      console.log("Generated Question:", result);
-      // Use the generated question as needed in your code
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-// });
+
+//   // Example usage
+//   generateQuestion()
+//     .then((result) => {
+//       console.log("Generated Question:", result);
+//       // Use the generated question as needed in your code
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+document.addEventListener("DOMContentLoaded", function() {
+  const checkAnswerButton = document.getElementById("checkAnswer");
+  checkAnswerButton.addEventListener("click", checkAnswer);
+  const toggleLikeButton = document.getElementById("toggleLike");
+  toggleLikeButton.addEventListener("click", redirectToGitHub);
+  const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+  toggleDarkModeButton.addEventListener("click", toggleDarkMode);
+  const toggleHamburgerButton = document.getElementById("toggleHamburger");
+  toggleHamburgerButton.addEventListener("click", toggleHamburgerMenu);
+  
+  const topicSelect = document.getElementById("topicSelect");
+  topicSelect.addEventListener("change", generateQuestion);
+
+  function toggleHamburgerMenu() {
+    const hamburgerMenuContent = document.querySelector(".hamburger-menu-content");
+    hamburgerMenuContent.classList.toggle("show");
+  }
+
+  function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  }
+
+  function generateQuestion() {
+    let currentTopic = topicSelect.value;
+    console.log(currentTopic);
+    fetch(`https://gauravkumarchaurasiya.github.io/codeQuiz/${currentTopic}.json`)
+      .then(response => response.json())
+      .then(data => {
+        const questionObjects = data;
+    
+        // Get a random question object
+        const randomQuestionObj = questionObjects[Math.floor(Math.random() * questionObjects.length)];
+    
+        const question = randomQuestionObj.question;
+        if(currentTopic=="python"){
+
+          const codeSnippet = randomQuestionObj.code;
+          document.getElementById("codeSnippet").textContent = codeSnippet;
+        }else{
+
+          const codeSnippet = randomQuestionObj.codeSnippet;
+          document.getElementById("codeSnippet").textContent = codeSnippet;
+        }
+        const correctAnswer = randomQuestionObj.output;
+        const description = randomQuestionObj.description;
+    
+        // Update the HTML elements
+        // document.getElementById("questionText").textContent = question;
+        document.getElementById("correctAnswer").textContent = correctAnswer;
+        document.getElementById("descriptionText").textContent = description;
+    
+        // console.log("Generated Question:", question,codeSnippet,correctAnswer);
+        // Use the generated question as needed in your code
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
+
+  function redirectToGitHub() {
+    window.open("https://github.com/gauravkumarchaurasiya/codequiz", "_blank");
+  }
+});
